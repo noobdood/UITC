d6de0bddc3fa7753f87d893b202415e8134957dd
new <new@news-Mac-mini.local>
2014-08-26 14:27:39 -0400

    solved the challenge

---
+++ UITableViewController Challenge/UITCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UITCTableViewController.h
+//  UITableViewController Challenge
+//
+//  Created by new on 8/26/14.
+//  Copyright (c) 2014 Yup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITCTableViewController : UITableViewController
+
+@end
