53bb75d9f010ed17c9b5b8227257f68f753c9593
new <new@news-Mac-mini.local>
2014-08-26 13:53:33 -0400

    Initial Commit

---
+++ UITableViewController Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge
+//
+//  Created by new on 8/26/14.
+//  Copyright (c) 2014 Yup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UITCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UITCAppDelegate class]));
+    }
+}
